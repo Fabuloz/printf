633b6c7ae2068a2d8cf77a2236fd325967e86eea
Leon-0 <36256754+Leon-0@users.noreply.github.com>
2023-01-24 10:26:12 +0100

    Returns the number of char printed

--- print_bases.c
+++ print_bases.c
@@ -66,7 +66,7 @@ int print_binary(va_list l, flags_t *f)
  * print_octal - prints a number in base 8
  * @l: va_list arguments from _printf
  * @f: pointer to the struct that determines
- * if a flag is passed to _printf
+ * if a flag is passed to _printf.
  * Description: the function calls convert() which in turns converts the input
  * number into the correct base and returns it as a string
  * Return: the number of char printed
